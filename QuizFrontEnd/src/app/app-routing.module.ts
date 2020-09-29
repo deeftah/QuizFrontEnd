f91f84ded7f5a3f461a34839f1ca108202bfbc4f
adrianabocse <adryannastef@gmail.com>
2020-09-29 19:47:59 +0300

    Add second component

--- QuizFrontEnd/src/app/app-routing.module.ts
+++ QuizFrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OptionListComponent } from './option-list/option-list.component';
+import { QuizAddComponent } from './quiz-add/quiz-add.component';
 
 
 const routes: Routes = [
-  {path:' ',component: OptionListComponent}
+  {path:' ',component: OptionListComponent},
+  {path:'quiz-add', component:QuizAddComponent}
 ];
 
 @NgModule({
--- QuizFrontEnd/src/app/app.module.ts
+++ QuizFrontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OptionListComponent } from './option-list/option-list.component';
+import { QuizAddComponent } from './quiz-add/quiz-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OptionListComponent
+    OptionListComponent,
+    QuizAddComponent
   ],
   imports: [
     BrowserModule,
