3ab0a172e6e257cca38a2aa26820f2dfc590486a
adrianabocse <adryannastef@gmail.com>
2020-09-30 23:51:07 +0300

    update

--- QuizFrontEnd/src/app/user-response/user-response.component.ts
+++ QuizFrontEnd/src/app/user-response/user-response.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SurveyData } from '../survey-data';
 
 @Component({
   selector: 'app-user-response',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserResponseComponent implements OnInit {
 
+  surveys: SurveyData[]=[
+    {idSurvey:1, titlesSurvey:'Fizica', expirationDateSurvey:new Date(),creationDateSurvey:new Date()},
+    {idSurvey:2, titlesSurvey:'Matematica',expirationDateSurvey:new Date(),creationDateSurvey:new Date()},
+    {idSurvey:3, titlesSurvey:'Engleza',expirationDateSurvey:new Date(),creationDateSurvey:new Date()},
+    {idSurvey:4, titlesSurvey:'Franceza',expirationDateSurvey:new Date(),creationDateSurvey:new Date()}
+    
+  ]
   constructor() { }
 
   ngOnInit(): void {
